0151102c2d7ab78556b39db20098a0d7f1408edd
Shivank Chaudhary <81817735+Alt-Shivam@users.noreply.github.com>
2022-09-17 22:51:33 +0530

    Create GetTrex.sh

---
+++ Tgen: TREX/Trex/GetTrex.sh
@@ -0,0 +1,8 @@
+sudo apt -y install zlib1g-dev build-essential python python3-distutils
+git clone https://github.com/cisco-system-traffic-generator/trex-core.git
+cd trex-core/
+cd linux_dpdk
+./b configure
+./b build
+cd ..
+sudo cp scripts/cfg/simple_cfg.yaml /etc/trex_cfg.yaml
